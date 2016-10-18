e1c679bce04583178fa59d0c71b7cdd4d90ef7bf
ChrisRose <crose@netflix.com>
2016-10-18 16:23:17 -0700

    More tests

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,11 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 import { TodoComponent } from './todo.component';
-import { FormsModule } from '@angular/forms';
 
+import { FormsModule } from '@angular/forms';
 import { Todo } from '../todo';
 
-var fixture, comp;
+let fixture: ComponentFixture<TodoComponent>;
+let comp: TodoComponent;
+let de: DebugElement;
+let el: HTMLElement;
+let destroyEl: DebugElement;
+let todo: Todo;
 
 beforeEach(() => {
   // refine the test module by declaring the test component
@@ -20,11 +26,11 @@ beforeEach(() => {
   // get test component from the fixture
   comp = fixture.componentInstance;
 
-  var todo: Todo = {
+  todo = {
     id: 1,
     name: 'Watch Luke Cage',
     completed: false
-  }
+  };
   comp.todo = todo;
 });
 
@@ -37,8 +43,27 @@ describe('Component: Todo', () => {
   it('displays the todo name', () => {
     // trigger change detection to update the view
     fixture.detectChanges();
-    // query for the title <h1> by CSS element selector
-    var de = fixture.debugElement.query(By.css('label'));
-    expect(de.nativeElement.textContent).toContain(comp.todo.name);
+    // query for the name <label> by CSS element selector
+    de = fixture.debugElement.query(By.css('label'));
+    el = de.nativeElement;
+    expect(el.textContent).toContain(comp.todo.name);
+  });
+
+  it('should raise onRemoveToDo when todo is removed', () => {
+    let selectedTodo: Todo;
+    comp.onRemoveTodo.subscribe((todo: Todo) => selectedTodo = todo);
+    destroyEl = fixture.debugElement.query(By.css('.destroy'));
+    destroyEl.triggerEventHandler('click', null);
+    expect(selectedTodo).toBe(todo);
+  });
+
+  it('should raise onToggleToDo when todo is toggled', () => {
+    let selectedTodo: Todo;
+    comp.onToggleTodo.subscribe((todo: Todo) => selectedTodo = todo);
+    destroyEl = fixture.debugElement.query(By.css('.toggle'));
+    destroyEl.triggerEventHandler('change', {
+      target: destroyEl.nativeElement
+    });
+    expect(selectedTodo).toBe(todo);
   });
 });
